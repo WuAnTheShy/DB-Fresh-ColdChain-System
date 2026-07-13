1989a30efb03981d678e4eede5bae966fb882145
GreyBlue0122 <1433804983@qq.com>
2026-07-13 12:12:32 +0800

    feat: 初始化项目框架，调整前端全局页布局
    1. 按照ASP.NET Core MVC模板，初始化构建了项目框架。
    2. 修改前端全局页布局标题、导航栏以及主界面文字，导航栏添加”登录“选项。

---
+++ Services/DbHelper.cs
@@ -0,0 +1,17 @@
+﻿using Dapper;
+using Oracle.ManagedDataAccess.Client;
+using Microsoft.Extensions.Configuration;
+public class DbHelper
+{
+    private readonly string _connectionString;
+    public DbHelper(IConfiguration config)
+    {
+        _connectionString = config.GetConnectionString("OracleDb");
+    }
+
+    // 供所有 Service 调用的执行 SQL 方法
+    public OracleConnection GetConnection()
+    {
+        return new OracleConnection(_connectionString);
+    }
+}
