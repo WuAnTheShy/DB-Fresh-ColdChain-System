426d4f8de3687c9f651feb2f1c38d1fcd5444391
Haocheng Liang <114935080@qq.com>
2026-07-24 16:33:37 +0800

    Add FreshGroupSystem

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FreshGroupSystem.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
